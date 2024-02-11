5299d5bad1f086ea34f526d448d6d9c933d637ed
romanykw <romanykw@msu.edu>
2024-02-11 14:59:50 -0500

    add Lab3Deliverables

---
+++ Lab3_Deliverables.R
@@ -0,0 +1,23 @@
+#Questions 
+
+#1. What is the difference between the sf library and the terra library 
+
+#2. There are some common spatial data manipulation functions (GIS shapefile manipulation functions) in the sf library. What are they and what do they do? 
+  
+#3. How would you create a new sf object representing a line or a polygon, given a set of coordinates? 
+  
+#4. How do you filter spatial data based on a specific attribute, such as area or population? 
+  
+#5. Suppose you have a dataset of weather stations and temperature readings. How would you visually represent areas of high and low temperatures on a map using R? 
+  
+#Your turn to code 
+
+#1. Create your own mixed geometry type with point, line, and polygon geometry and apply a buffer 
+
+#2. Create two polygons and perform an union that combines both of them 
+
+#3. Create a mult-layer raster object and produce a jpg output (jpg output is not in the R code – you’ll have to google/Chat GPT how to use it) 
+
+#4. The code provides a couple of ways to represent trees as a geographic phenomena. Please write code to represent trees in a different way. Be creative! 
+  
+#5. There are several examples of Choropleth maps that use different classification techniques. Please provide a Choropleth map of a variable in the Marriage Dataset that uses a sequential color scheme.  
